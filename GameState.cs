86f7739b1ff994a1a4198f4d21eddc21631954a9
Jade Ferguson <jadesnider2440@gmail.com>
2023-11-27 16:03:45 -0800

    Made Class to get position of snake, add food to the game

---
+++ GameState.cs
@@ -0,0 +1,91 @@
+﻿
+
+using System;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+
+namespace SnakeGame
+{
+    public class GameState
+    {
+        public int Rows { get; }
+        public int Cols { get; }
+        public GridValue[,] Grid { get; }
+        public Direction Dir { get; private set; }
+        public int Score { get; private set; }
+        public bool GameOver { get; private set; }
+
+        // List of postions occupied by the snake
+        private readonly LinkedList<Position> snakePositions = new LinkedList<Position>();
+        // Used to figure out where the food should respond
+        private readonly Random random = new Random();
+
+        public GameState(int rows, int cols)
+        {
+            Rows = rows;
+            Cols = cols;
+            Grid = new GridValue[Rows, Cols];
+            Dir = Direction.Right;
+
+            AddSnake();
+            AddFood();
+        }
+        
+        private void AddSnake()
+        {
+            int r = Rows / 2;
+
+            // Loop over cols
+            for (int c = 1; c <=3; c++)
+            {
+                Grid[r, c] = GridValue.Snake;
+                snakePositions.AddFirst(new Position(r, c));
+            }
+        }
+
+        // Make food for snake to grow
+        private IEnumerable<Position> EmptyPositions ()
+        {
+            for (int r = 0; r < Rows; r++)
+            {
+                for (int c = 0; c < Cols; c++)
+                {
+                    if (Grid[r, c] == GridValue.Empty)
+                    {
+                        yield return new Position(r, c);
+                    }
+                }
+            }
+        }
+        private void AddFood()
+        {
+            List<Position> empty = new List<Position>(EmptyPositions());
+
+            if (empty.Count == 0)
+            {
+                return;
+            }
+
+            Position pos = empty[random.Next(empty.Count)];
+            Grid[pos.Row, pos.Col] = GridValue.Food;
+
+        }
+
+        // Helper methods for snake head and snake tail positions
+
+        public Position HeadPosition()
+        {
+            return snakePositions.First.Value;
+        }
+
+        public Position TailPosition()
+        {
+            return snakePositions.Last.Value;
+        }
+
+        public IEnumerable<Position> SnakePosition()
+        {
+            return snakePositions;
+        }
+    }
+}
